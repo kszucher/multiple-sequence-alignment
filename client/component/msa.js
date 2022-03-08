1f9e095a509fb32ce0b7104685c77de431755c0a
Krisztian Szucher <krisztian@mindboard.io>
2022-03-08 20:08:52 +0100

    occurence

--- client/component/msa.js
+++ client/component/msa.js
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react'
 import  species  from './species.json'
 import  nucleotides  from './nucleotides.json'
 
-const VIEWBOX_A = {w:390, h:552}
-const VIEWBOX_B = {w:300, h:552}
-const VIEWBOX_C = {w:792, h:552}
+const VIEWBOX_A = {w:260, h:552}
+const VIEWBOX_B = {w:300, h:VIEWBOX_A.h}
+const VIEWBOX_C = {w:792, h:VIEWBOX_A.h}
+const VIEWBOX_D = {w:VIEWBOX_A.w+VIEWBOX_B.w, h: 100}
+const VIEWBOX_E = {w:VIEWBOX_C.w, h:VIEWBOX_D.h}
 const LETTER_SIZE = 24
 const FONT_SIZE = 16
-const PATH_DISTANCE = 30
+const PATH_DISTANCE = 20
+
+const da = {display: 'flex', alignItems: 'center'}
+const pb = {padding: 10, backgroundColor: '#FFF'}
+const svgFont = {fontFamily:"Roboto", fontSize:FONT_SIZE, fill:"#000", textAnchor:"middle", dominantBaseline:"middle"}
 
 const nucleotidColors = {
     '-': '#FFFFFF',
@@ -46,6 +52,12 @@ const linspace = (start, stop, num, endpoint = true) => {
     const step = (stop - start) / div;
     return Array.from({length: num}, (_, i) => start + step * i);
 }
+
+const transpose = (matrix) => matrix.reduce((prev, next) => next.map((item, i) => (prev[i] || []).concat(next[i])), [])
+
+const occurrences = (arr) => arr.reduce( (acc, curr) => (acc[curr] ? ++acc[curr] : acc[curr] = 1, acc), {})
+
+const maxOccurence = (obj, ) => Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b)
 
 const getEdgePath = ([x1, y1, x2, y2]) => {
     const dx = PATH_DISTANCE
@@ -165,6 +177,29 @@ const SpeciesPathVisualizer = ({pathDescriptors, highlightDescriptors}) => {
     )
 }
 
+const NameVisualizer = ({nameHover, highlightIdx}) => {
+    return (
+        <svg viewBox={`0 0 ${VIEWBOX_B.w} ${VIEWBOX_B.h}`} style={{backgroundColor: '#FFFFFF'}}>
+            {nucleotides.map((el, idx) => (
+                <text
+                    x={20}
+                    y={idx*LETTER_SIZE + LETTER_SIZE / 2}
+                    key={'text' + idx}
+                    fontFamily="Roboto"
+                    fontSize={FONT_SIZE}
+                    fontWeight={idx===highlightIdx ? 'bold' : 'normal'}
+                    fill="black"
+                    textAnchor="start"
+                    dominantBaseline="middle"
+                    onMouseEnter={() => nameHover(el.name, idx)}
+                >
+                    {el.name}
+                </text>
+            ))}
+        </svg>
+    )
+}
+
 const NucleotidVisualizer = ({nucleotidMat, highlightIdx}) => {
     const horzLen = nucleotidMat[0]?.length
     const vertLen = nucleotidMat?.length
@@ -172,38 +207,43 @@ const NucleotidVisualizer = ({nucleotidMat, highlightIdx}) => {
     const vertLinSpace = linspace(0, LETTER_SIZE*vertLen, vertLen, false)
     const letterDescriptors = nucleotidMat
         .map((iEl, iIdx) => iEl
-            .map((jEl, jIdx) => {
-                return {
+            .map((jEl, jIdx) => ({
                     x: horzLinSpace[jIdx],
                     y: vertLinSpace[iIdx],
                     c: nucleotidMat[iIdx][jIdx],
-                    fw: iIdx===highlightIdx ? 'bold' : 'normal'
-                }})).flat()
+                    fw: iIdx === highlightIdx ? 'bold' : 'normal'
+                }))).flat()
     return (
-        <svg viewBox={`0 0 ${VIEWBOX_C.w} ${VIEWBOX_C.h}`} style={{backgroundColor: '#dddddd'}}>
-            {letterDescriptors.map((el, idx) => (
+        <svg viewBox={`0 0 ${VIEWBOX_C.w} ${VIEWBOX_C.h}`} style={{}}>
+            {letterDescriptors.map(({x, y, c, fw}, idx) => (
                 <g key={idx}>
-                    <rect
-                        x={el.x}
-                        y={el.y}
-                        width={LETTER_SIZE}
-                        height={LETTER_SIZE}
-                        key={'rect' + el.x*10 + el.y}
-                        fill={nucleotidColors[el.c]}
-                    >
+                    <rect x={x} y={y} width={LETTER_SIZE} height={LETTER_SIZE} key={Math.random()} fill={nucleotidColors[c]}>
                     </rect>
-                    <text
-                        x={el.x + LETTER_SIZE / 2}
-                        y={el.y + LETTER_SIZE / 2 + 3}
-                        key={'text' + el.x*10 + el.y}
-                        fontFamily="Roboto"
-                        fontSize={FONT_SIZE}
-                        fontWeight={el.fw}
-                        fill="#000"
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                    >
-                        {el.c}
+                    <text x={x + LETTER_SIZE / 2} y={y + LETTER_SIZE / 2 + 3} key={Math.random()} fontWeight={fw} {...svgFont}>
+                        {c}
+                    </text>
+                </g>
+            ))}
+        </svg>
+    )
+}
+
+const ResultVisualizer = ({mostFrequentNucleotidMat}) => {
+    const horzLen = mostFrequentNucleotidMat.length
+    const horzLinSpace = linspace(0, LETTER_SIZE*horzLen, horzLen, false)
+    const resultDescriptors = mostFrequentNucleotidMat.map((el, idx) => ({
+        x: horzLinSpace[idx],
+        c: maxOccurence(el),
+        h: el[maxOccurence(el)]
+    }))
+    return (
+        <svg viewBox={`0 0 ${VIEWBOX_E.w} ${VIEWBOX_E.h}`} style={{}}>
+            {resultDescriptors.map(({x, y, h, c}, idx) => (
+                <g key={idx}>
+                    <rect x={x} y={y} width={LETTER_SIZE} height={h*4} key={Math.random()} fill={nucleotidColors[c]}>
+                    </rect>
+                    <text x={x + LETTER_SIZE / 2} y={LETTER_SIZE / 2} key={'text' + x*10 + y}{...svgFont}>
+                        {c}
                     </text>
                 </g>
             ))}
@@ -213,6 +253,7 @@ const NucleotidVisualizer = ({nucleotidMat, highlightIdx}) => {
 
 export function Msa () {
     const [nucleotidMat, setNucleotidMat] = useState([])
+    const [mostFrequentNucleotidMat, setMostFrequentNucleotidMat] = useState([])
     const [pathDescriptors, setPathDescriptors] = useState([])
     const [highlightIdx, setHighlightIdx] = useState('')
     const [highlightDescriptors, setHighlightDescriptors] = useState([])
@@ -226,63 +267,29 @@ export function Msa () {
         setPathDescriptors(speciesProcessor.pathDescriptors)
         setHighlightDescriptors(speciesProcessor.highlightDescriptors)
         setNucleotidMat(speciesProcessor.nucleotidMat)
+        setMostFrequentNucleotidMat(transpose(speciesProcessor.nucleotidMat).map(el => occurrences(el)))
     }, [])
     return (
-        <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-            paddingTop: 100,
-        }}>
-            <div style={{
-                fontSize: 30,
-                fontFamily: 'Roboto',
-                padding: 20
-            }}>
-                MULTI SEQUENCE ALIGNMENT
+        <div style={{ ...da, flexDirection: 'column', paddingTop: 100, flexWrap: 'wrap',  }}>
+            <div style={{ ...pb, borderRadius: '16px 16px 0 0', fontSize: 30, fontFamily: 'Roboto' }}>
+                {'MULTI SEQUENCE ALIGNMENT'}
             </div>
-            <div
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    width: VIEWBOX_A.w + VIEWBOX_B.w + VIEWBOX_C.w,
-                    height: VIEWBOX_A.h,
-                    padding: 10,
-                    backgroundColor: '#FFF',
-                    borderRadius: 16,
-                    cursor: 'pointer'
-                }}>
+            <div style={{ ...da, ...pb, borderRadius: '16px 16px 0 16px', cursor: 'pointer' }}>
                 <div style={{ width: VIEWBOX_A.w, height: VIEWBOX_A.h }}>
-                    {pathDescriptors.length && <SpeciesPathVisualizer
-                        pathDescriptors={pathDescriptors}
-                        highlightDescriptors={highlightDescriptors}
-                    />}
+                    <SpeciesPathVisualizer {...{pathDescriptors}} {...{highlightDescriptors}}/>
                 </div>
                 <div style={{ width: VIEWBOX_B.w, height: VIEWBOX_B.h }}>
-                    <svg viewBox={`0 0 ${VIEWBOX_B.w} ${VIEWBOX_B.h}`} style={{backgroundColor: '#FFFFFF'}}>
-                        {nucleotides.map((el, idx) => (
-                            <text
-                                x={20}
-                                y={idx*LETTER_SIZE + LETTER_SIZE / 2}
-                                key={'text' + idx}
-                                fontFamily="Roboto"
-                                fontSize={FONT_SIZE}
-                                fontWeight={idx===highlightIdx ? 'bold' : 'normal'}
-                                fill="black"
-                                textAnchor="start"
-                                dominantBaseline="middle"
-                                onMouseEnter={() => nameHover(el.name, idx)}
-                            >
-                                {el.name}
-                            </text>
-                        ))}
-                    </svg>
+                    <NameVisualizer  {...{nameHover}} {...{highlightIdx}} />
                 </div>
                 <div style={{ width: VIEWBOX_C.w, height: VIEWBOX_C.h }}>
-                    <NucleotidVisualizer
-                        nucleotidMat={nucleotidMat}
-                        highlightIdx={highlightIdx}
-                    />
+                    <NucleotidVisualizer {...{nucleotidMat}} {...{highlightIdx}}/>
+                </div>
+            </div>
+            <div style={{ ...da, cursor: 'pointer' }}>
+                <div style={{ width: VIEWBOX_D.w, height: VIEWBOX_D.h }}>
+                </div>
+                <div style={{ ...pb, borderRadius: '0 0 16px 16px', width: VIEWBOX_E.w, height: VIEWBOX_E.h }}>
+                    <ResultVisualizer {...{mostFrequentNucleotidMat}}/>
                 </div>
             </div>
         </div>
